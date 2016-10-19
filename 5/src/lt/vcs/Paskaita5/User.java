1ecfd022b9da12f18d945f36279802fe49a641f5
EMEA\KRIVIJ <ok@ok.lt>
2016-10-19 19:57:02 +0300

    Signed-off-by: EMEA\KRIVIJ <ok@ok.lt>

--- 5/src/lt/vcs/Paskaita5/User.java
+++ 5/src/lt/vcs/Paskaita5/User.java
@@ -8,6 +8,19 @@ public class User {
     private String vardas;
     private String pavarde;
     private final String klasesVardas;
+    private  int amzius = 77;
+
+    public int getAmzius() {
+        return amzius;
+    }
+    public int getAmzius(int plius){
+    return amzius + plius;
+    }
+
+    public int getAmzius(String plius){
+        return amzius + new Integer(plius);
+    }
+
 
     /**
      * User klases konstruktorius
@@ -16,15 +29,23 @@ public class User {
      */
 
     public User(String vardas, String pavarde){
-        this.vardas = vardas;
+        this(vardas);
         this.pavarde = pavarde;
+//        this.klasesVardas = this.getClass().getName();
+    }
+
+    public User(String vardas){
+        super();
+        this.vardas = vardas;
         this.klasesVardas = this.getClass().getName();
     }
 
     @Override
     public String toString(){
-        String result = this.klasesVardas + ": " +
-                " vardas: "+ this.vardas +
+        //this.klasesVardas + ": " +//
+        String result = "super: " + super.toString()+
+                " "+ this.klasesVardas+ ";"
++        " vardas: "+ this.vardas +
                 " pavarde: "+ this.pavarde;
         return  result;
     }
